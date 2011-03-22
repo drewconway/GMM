2083dc1ba3a6ace40497080aba0912fbeb720d0d
Drew Conway <agconway@gmail.com>
2011-03-22 16:12:09 -0400

    Updated nominally to 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='GMM',
-    version='0.3.1',
+    version='0.3.2',
     author='Drew Conway',
     author_email='drew.conway@nyu.edu',
     packages=['gmm', 'gmm.test'],
@@ -11,5 +11,5 @@ setup(
     url='http://pypi.python.org/pypi/GMM/',
     license='LICENSE.txt',
     description='This package provides a basic framework and supporting functionality for generating network structure network structure using graph motifs.',
-    long_description=open('README.txt').read(),
+    long_description=open('README').read(),
 )
